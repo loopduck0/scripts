e2c4b48d3cc1513c56b2d479fbfc203fbf7a8df3
loopduck0 <loopduck0@gmail.com>
2021-07-10 17:06:26 +0000

    Placeholder install.sh and some git config changes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+BOOTSTRAP_DIR='~/'
+
 # Installs gh tool for Debian/Ubuntu following (as of 10 July 2021)
 # https://github.com/cli/cli/blob/trunk/docs/install_linux.md
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
@@ -9,9 +11,15 @@ sudo apt install gh
 
 # Clone loopduck0/scripts
 gh auth login  # https://docs.github.com/en/github/authenticating-to-github/keeping-your-account-and-data-secure/creating-a-personal-access-token#creating-a-token
+cd $BOOTSTRAP_DIR
 gh repo clone loopduck0/scripts
 
 # Setup git configs
 git config --global user.email "loopduck0@gmail.com"
 git config --global user.name "loopduck0"
 git config --global core.editor vim
+git config --global color.ui true
+
+# cd to repo and run install script
+cd $BOOTSTRAP_DIR/scripts
+./install.sh
