50ab4927a6813e55c39481543773511d1ecee53c
loopduck0 <loopduck0@gmail.com>
2021-07-11 03:49:13 +0000

    Initial irssi install and bashrc configs

---
+++ bashrc_config.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Input file
+input=$1
+
+# Output file
+config_file=$2
+
+# Write file
+if [ -f "$config_file" ]; then
+    echo "Overwriting $config_file..."
+    # echo "WARNING: $config_file already exists. Would you like to overwrite this file? (Y/n)" 
+fi
+
+cp -f $input $config_file
+chmod a+x $config_file
+
+# Add to bashrc
+if grep -q "$config_file\$" ~/.bashrc; then
+    echo "$config_file is already in bashrc"
+else
+    echo "Adding $config_file to bashrc"
+    echo "source $config_file" >> ~/.bashrc
+fi
--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "Placeholder install.sh"
+# Install IRC client - irsii instead of weechat because fewer
+# dependencies
+sudo apt install irssi
+
+# Add files to bashrc
+mkdir -p ~/.loopduck0
+./bashrc_config.sh ./bashrc/aliases ~/.loopduck0/aliases
